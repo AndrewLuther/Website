5679469cc90bf7730b8a4feefd8ca55a5e15f97b
Andrew Luther <90206207+AndrewLuther@users.noreply.github.com>
2022-05-12 14:13:19 -0230

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Todo from './components/Todo';
 function App() {
   return (
     <div>
-      <h1>My To dos</h1>
+      <h1>Andrew Luther</h1>
       <Todo text='Learn React' />
       <Todo text='Master React' />
       <Todo text='Explore the full React course' />
