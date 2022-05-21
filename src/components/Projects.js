ee4fc1edd27af654bec61060477ec5bdfac892e0
Andrew Luther <90206207+AndrewLuther@users.noreply.github.com>
2022-05-21 16:24:42 -0230

    edits

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,18 @@ function Projects() {
     return(
         <div>
             <h2>My Projects</h2>
-            <p></p>
+            
+            <h3>Kingdomino</h3>
+            <h4>Resources: Java</h4>
+            <p>Kingdomino is a digital version of the board game, developed in Java. 
+            This was a group project with Ayon Debnath, Sahil Mahey, Benjamin Stanley, and Muhammad Bilal Naseer.</p>
+            <br/>
+            <h3>Chord Progression Generator</h3>
+            <h4>Resources: Java</h4>
+            <p>Chord Progression Generator outputs a random list of chords 
+                in any user-defined major or minor scale (e.g. C major).</p>
+
+            
         </div>
     );
 
