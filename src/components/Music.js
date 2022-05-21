a8360f2494e2e73896484dbb0331f0206aebde4e
Andrew Luther <90206207+AndrewLuther@users.noreply.github.com>
2022-05-21 18:44:22 -0230

    started music page

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,7 +1,15 @@
+import React from 'react';
+
 function Music() {
     return (
     <div>
         <h2>Videos</h2>
+        <iframe src="https://www.youtube.com/watch?v=bLg7wZN7yuY"
+        frameborder='0'
+        allow='autoplay; encrypted-media'
+        allowFullScreen
+        title='Variations on Circular Motion'
+        />
     </div>
 
     );
